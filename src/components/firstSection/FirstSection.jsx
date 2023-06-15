d2a9f48f2161075b06dfff25e673cd1a5a1e7286
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-06-15 10:29:59 +0000

    First Commit

--- src/components/firstSection/FirstSection.jsx
+++ src/components/firstSection/FirstSection.jsx
@@ -20,26 +20,26 @@ export const FirstSection = () => {
     return(
         <div className="container-fluid p-0">
             <div className="row g-0">
-                <div className="col-md-6 col-sm-12 d-flex flex-column align-items-center justify-content-md-center my-5 my-md-0" data-aos="zoom-in-left">
+                <div className="col-md-6 col-sm-12 d-flex flex-column align-items-center justify-content-md-center my-5 my-md-0" data-aos="zoom-in">
                     <div className="w-75">
                         <h1>Transform your brand</h1>
                         <p className="my-lg-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, dignissimos accusantium! Eius illum eveniet perferendis obcaecati quos? </p>
                         <p className="fw-bold"><a href="#" className="text-dark learnMore1">LEARN MORE</a></p>
                     </div>
                 </div>
-                <div className="col-md-6 col-sm-12" data-aos="zoom-in-right">
+                <div className="col-md-6 col-sm-12" data-aos="flip-up">
                     <div>
                         <img src={oeuf} alt="" className="w-100"/>
                     </div>
                 </div>
             </div>
             <div className="row g-0">
-                <div className="col-md-6 col-sm-12" data-aos="zoom-in-left">
+                <div className="col-md-6 col-sm-12" data-aos="flip-up">
                     <div>
                         <img src={cup} alt="" className="w-100"/>
                     </div>
                 </div>
-                <div className="col-md-6 col-sm-12 d-flex flex-column align-items-center justify-content-md-center my-5 my-md-0" data-aos="zoom-in-right">
+                <div className="col-md-6 col-sm-12 d-flex flex-column align-items-center justify-content-md-center my-5 my-md-0" data-aos="zoom-in">
                     <div className="w-75">
                     <h1>Transform your brand</h1>
                         <p className="my-lg-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, dignissimos accusantium! Eius illum eveniet perferendis obcaecati quos? </p>
@@ -49,7 +49,7 @@ export const FirstSection = () => {
             </div>
 
             <div className="row g-0">
-                <div className="col-md-6 col-sm-12 position-relative" data-aos="zoom-in-left">
+                <div className="col-md-6 col-sm-12 position-relative" data-aos="flip-up">
                     <div className="position-relative">
                         <img src={raisin} alt="" className="w-100 position-absolute"/>
                         <div className="graphDesi position-absolute d-flex justify-content-center">
@@ -60,7 +60,7 @@ export const FirstSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-6 col-sm-12 position-relative colPhoto" data-aos="zoom-in-right">
+                <div className="col-md-6 col-sm-12 position-relative colPhoto" data-aos="flip-up">
                     <div className="position-relative">
                         <img src={orange} alt="" className="w-100 position-absolute"/>
                         <div className="photographique position-absolute d-flex justify-content-center">
